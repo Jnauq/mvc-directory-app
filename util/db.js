89758c0dc3490ff5e53c7e48d47ac6f4917b1909
Jnauq <justin.aq88@gmail.com>
2020-03-10 20:31:00 -0700

    heroku pg credentials

--- util/db.js
+++ util/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({  
-    host: 'localhost',  
-    user: 'postgres',  
-    database: 'web_test',  
-    password: 'root',
+    host: 'ec2-18-209-187-54.compute-1.amazonaws.com',  
+    user: 'xprbzbsspnzqns',  
+    database: 'dakcdolm15o21p',  
+    password: '0611d0700325758169c8975b70c81ea0b4ed5fb47b3394aa51280f1a0772ee5c',
     port: 5432,
     ssl: false
 });  
